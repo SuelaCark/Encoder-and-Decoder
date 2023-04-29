d0f43f40ff4b8300505588e4eb2a95dc2c1847db
SuelaCark <suela.qarkaxhija@gmail.com>
2023-04-29 13:06:07 +0300

    completed Caesar decrypt func

--- src/caesar_cipher.py
+++ src/caesar_cipher.py
@@ -1,6 +1,6 @@
 """
     This file holds the Caesar Cipher child class which inherits the variables
-    of abstractclass Cipher and overrides its abstract methods with the
+    of abstract class Cipher and overrides its abstract methods with the
     relevant implementation for the Caesar cipher.
 
 """
@@ -13,6 +13,9 @@ class CaesarCipher(Cipher):
     def __init__(self, key, shift):
         super().__init__(key, shift)  # this invokes the parent initializer
 
+    # For now, the encrypt func only encrypts text
+    # Might be able to expand it to also include numbers with shifting
+    # for a better encryption of the text
     def encrypt(self, text):
         encrypted_text = ""
         # transverse the plain text
@@ -22,12 +25,10 @@ class CaesarCipher(Cipher):
                 ascii_code = ord(char)
 
                 new_ascii_code = (ascii_code - shift_code + self._shift) % 26 + shift_code
-
                 encrypted_char = chr(new_ascii_code)
-                # encrypted_char = chr(ascii_code - shift_code + self._shift) % 26 + shift_code
-                # encrypted_text += encrypted_char
             else:
                 encrypted_char = char
+
             encrypted_text += encrypted_char
 
         return encrypted_text
@@ -37,11 +38,15 @@ class CaesarCipher(Cipher):
 
         for char in text:
             if char.isalpha():
-                # Shift the character by the inverse of the key
-                shifted_char = chr((ord(char) - 65 - self._shift) % 26 + 65)
-                decrypted_text += shifted_char
+                shift_code = 65 if char.isupper() else 97
+                ascii_code = ord(char)
+
+                new_ascii_code = (ascii_code - self._shift - shift_code) % 26 + shift_code
+                decrypted_char = chr(new_ascii_code)
             else:
-                decrypted_text += char
+                decrypted_char = char
+
+            decrypted_text += decrypted_char
 
         return decrypted_text
 
