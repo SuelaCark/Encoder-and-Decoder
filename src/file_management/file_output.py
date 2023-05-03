9388b7a837b767a8e3691578a5ff4ec5f0505f2c
SuelaCark <suela.qarkaxhija@gmail.com>
2023-05-03 10:20:20 +0300

    reformated file_output

--- src/file_management/file_output.py
+++ src/file_management/file_output.py
@@ -10,7 +10,7 @@ from src.ciphers.vignere_cipher import VignereCipher
 
 def process_input():
     # Choose operation/action: "encrypt" or "decrypt"
-    operation = str(input("Do you want to encrypt or decrypt the message? (encrypt/decrypt): "))
+    operation = input("Do you want to encrypt or decrypt the message? (encrypt/decrypt): ")
 
     # Exclude operations which do not match the
     if operation not in ["encrypt", "decrypt"]:
@@ -21,38 +21,37 @@ def process_input():
 
     # Map the cipher name to a cipher class and ask for the encryption/decryption key for each cipher respectively
     if cipher_name == "caesar":
-        key_prompt = 'Enter the encryption key: ' if operation == "encrypt" else 'Enter the decryption key: '
-        key = int(input('Enter the encryption key: '))
+        key_prompt = 'Enter the encryption key (integer expected): ' \
+            if operation == "encrypt" else 'Enter the decryption key (integer expected): '
+        key = int(input(key_prompt))
         cipher = CaesarCipher(key)
+        input_text = str(input("Enter the message you would like to encrypt or decrypt: "))
+        output_text = cipher.encrypt(input_text, key) if operation == "encrypt" else cipher.decrypt(input_text, key)
     elif cipher_name == "atbash":   # The AtbashCipher does not need a key
         cipher = AtbashCipher()
+        input_text = str(input("Enter the message you would like to encrypt or decrypt: "))
+        output_text = cipher.encrypt(input_text, key="") \
+            if operation == "encrypt" else cipher.decrypt(input_text, key="")
     elif cipher_name == "vignere":
-        key_prompt = 'Enter the encryption key: ' if operation == "encrypt" else 'Enter the decryption key: '
-        key = input('Enter the encryption key: ')
+        key_prompt = 'Enter the encryption key (string expected): ' \
+            if operation == "encrypt" else 'Enter the decryption key (string expected): '
+        key = input(key_prompt)
         cipher = VignereCipher(key)
+        input_text = str(input("Enter the message you would like to encrypt or decrypt: "))
+        output_text = cipher.encrypt(input_text, key) if operation == "encrypt" else cipher.decrypt(input_text, key)
     else:
         raise ValueError(f"Invalid cipher name: {cipher_name}")
 
-    # If the warning key does not effect the code logic, the following commented code can be deleted
-
     # Defining a list of all allowed cipher classes
-    # ALLOWED_CIPHERS = [CaesarCipher, AtbashCipher, VignereCipher]
+    ALLOWED_CIPHERS = [CaesarCipher, AtbashCipher, VignereCipher]
 
-    # if isinstance(AtbashCipher(), object):
-    #     pass
-    # if isinstance(cipher, tuple(ALLOWED_CIPHERS)):
-    #     key = int(input(key_prompt))
-    #     print(key_prompt)
-    # else:
-    #     raise ValueError(f"Invalid cipher: {cipher.__class__.__name__}")
-
-    input_text = str(input("Enter the message you would like to encrypt or decrypt: "))
+    if not isinstance(cipher, tuple(ALLOWED_CIPHERS)):
+        raise ValueError(f"Invalid cipher: {cipher.__class__.__name__}")
 
     filename = input('Enter the name of the output file: ')
-    output_text = cipher.encrypt(input_text, key) if operation == "encrypt" else cipher.decrypt(input_text, key)
 
     try:
-        with open(filename, 'w') as file:
+        with open(filename + ".txt", 'w') as file:
             file.write(output_text)
 
     except FileNotFoundError:
