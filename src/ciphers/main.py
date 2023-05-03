c37675b5ef417d20d937094d210fda184555ab27
SuelaCark <suela.qarkaxhija@gmail.com>
2023-05-03 03:24:25 +0300

    optimized the file_output

--- src/ciphers/main.py
+++ src/ciphers/main.py
@@ -10,7 +10,6 @@ TODO:   2. File writer + connect it to the ciphers
         10. Video
 '''
 
-# from src.ciphers.file_output import FileReader
 from src.ciphers.caesar_cipher import CaesarCipher
 from src.file_management.file_output import process_input
 
@@ -25,29 +24,6 @@ if __name__ == '__main__':
         Note: To store the file names you could use either constants or configuration file ( configparser to parse it). 
         Or you could pass them when you run the python script and use argparse for that.
     """
-
-
-    # cipher_name = str(input("Please enter one of the following ciphers which you would like to use: "
-    #                         "Caesar, Atbash, Vignere: "))
-    #
-    # if cipher_name == "Caesar":
-    #     ...
-    # elif cipher_name == "Atbash":
-    #     ...
-    # elif cipher_name == "Vignere":
-    #     ...
-    # else:
-    #     raise ValueError("The available ciphers are: Caesar, Atbash, and Vignere")
-
-    # file_reader = FileReader(file_name)
-    # file_reader.read_file(file_name)
-
-    # file_name = str(input("Please input your existing file name: "))
-    # mode = str(input("Please enter 'encrypt' (e) if you wish to encrypt the file contents, or enter 'decrypt' (d) "
-    #                  "if you wish to decrypt the file contents: "))
-    # cipher_name = str(input("Please enter one of the following ciphers which you would like to use: "
-    #                         "Caesar, Atbash, Vignere: "))
-
 
 
     # # Testing Caesar Cipher
--- src/file_management/file_output.py
+++ src/file_management/file_output.py
@@ -1,10 +1,7 @@
-# This file manages the file input file and handles it to the respective cipher for encryption
-# of the file contents
-
-# The input/output files can be managed and structure in 2 ways:
-# 1. The main Cipher class can manage the file input and output, while the children classes access
-#   and/or override these methods (better code optimization)
-# 2. There can be a separate input and output file for file management before and after encryption
+"""
+    This file manages the file input file and handles it to the respective cipher for encryption
+    of the file contents
+"""
 
 from src.ciphers.caesar_cipher import CaesarCipher
 from src.ciphers.atbash_cipher import AtbashCipher
@@ -15,41 +12,41 @@ def process_input():
     # Choose operation/action: "encrypt" or "decrypt"
     operation = str(input("Do you want to encrypt or decrypt the message? (encrypt/decrypt): "))
 
-    if operation == "encrypt":
-        key_prompt = 'Enter the encryption key: '
-    elif operation == "decrypt":
-        key_prompt = 'Enter the decryption key: '
-    else:
+    # Exclude operations which do not match the
+    if operation not in ["encrypt", "decrypt"]:
         raise ValueError(f"Invalid operation: {operation}")
 
     # Write the name of the cipher the user wants to use
     cipher_name = input('Enter the cipher name ("caesar", "atbash", or "vignere"): ')
 
+    # Map the cipher name to a cipher class and ask for the encryption/decryption key for each cipher respectively
     if cipher_name == "caesar":
-        cipher = CaesarCipher()
-    elif cipher_name == "atbash":
+        key_prompt = 'Enter the encryption key: ' if operation == "encrypt" else 'Enter the decryption key: '
+        key = int(input('Enter the encryption key: '))
+        cipher = CaesarCipher(key)
+    elif cipher_name == "atbash":   # The AtbashCipher does not need a key
         cipher = AtbashCipher()
     elif cipher_name == "vignere":
-        cipher = VignereCipher()
+        key_prompt = 'Enter the encryption key: ' if operation == "encrypt" else 'Enter the decryption key: '
+        key = input('Enter the encryption key: ')
+        cipher = VignereCipher(key)
     else:
         raise ValueError(f"Invalid cipher name: {cipher_name}")
 
-    # Defining a list of all allowed cipher classes
-    ALLOWED_CIPHERS = [CaesarCipher, AtbashCipher, VignereCipher]
+    # If the warning key does not effect the code logic, the following commented code can be deleted
 
-    if isinstance(AtbashCipher(), object):
-        pass
-    if isinstance(cipher, tuple(ALLOWED_CIPHERS)):
-        key = int(input(key_prompt))
-    else:
-        raise ValueError(f"Invalid cipher: {cipher.__class__.__name__}")
+    # Defining a list of all allowed cipher classes
+    # ALLOWED_CIPHERS = [CaesarCipher, AtbashCipher, VignereCipher]
+
+    # if isinstance(AtbashCipher(), object):
+    #     pass
+    # if isinstance(cipher, tuple(ALLOWED_CIPHERS)):
+    #     key = int(input(key_prompt))
+    #     print(key_prompt)
+    # else:
+    #     raise ValueError(f"Invalid cipher: {cipher.__class__.__name__}")
 
     input_text = str(input("Enter the message you would like to encrypt or decrypt: "))
-
-    if operation == 'encrypt':
-        cipher = CaesarCipher(key)
-    else:
-        cipher = CaesarCipher(key)
 
     filename = input('Enter the name of the output file: ')
     output_text = cipher.encrypt(input_text, key) if operation == "encrypt" else cipher.decrypt(input_text, key)
@@ -59,4 +56,4 @@ def process_input():
             file.write(output_text)
 
     except FileNotFoundError:
-        print("No such file")
+        print("File not found. There is no such file")
