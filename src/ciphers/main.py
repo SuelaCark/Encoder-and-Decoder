4148aa1c18f3eed146705927ebbdc29e799ad16b
SuelaCark <suela.qarkaxhija@gmail.com>
2023-05-03 15:40:45 +0300

    removed commented code

--- src/ciphers/main.py
+++ src/ciphers/main.py
@@ -5,7 +5,6 @@ TODO:   1. Comments of all files and what they do
         4. Video
 '''
 
-from src.ciphers.caesar_cipher import CaesarCipher
 from src.file_management.file_output import process_input
 
 
@@ -14,30 +13,3 @@ if __name__ == '__main__':
           "This program will allow you to encrypt and decrypt messages in existing files within the project directory.")
     process_input()
 
-
-    # # Testing Caesar Cipher
-    # cipher = CaesarCipher(7)
-    # encrypted_message = cipher.encrypt("Hello world!", 7)
-    # decrypted_message = cipher.decrypt(encrypted_message)
-    #
-    # print(encrypted_message)
-    # print(decrypted_message)
-    #
-    # # Testing Playfair Cipher
-    # cipher2 = PlayfairCipher("Gravity")
-    # encrypted_message2 = cipher2.encrypt("HELLO WORLD", "Gravity")
-    #
-    # # Testing Atbash Cipher
-    # plaintext = "HELLO World"
-    # cipher3 = AtbashCipher()
-    #
-    # print(cipher3.encrypt(plaintext))
-    # print(cipher3.decrypt("SVOOL Dliow53212   ??!"))
-    #
-    # # Testing Vignere Cipher
-    # plain_text = "sunday"
-    # key = "michigan"
-    # cipher4 = VignereCipher("michigan")
-    # text = cipher4.encrypt(plain_text, key)
-    # print(text)
-    # print(cipher4.decrypt(text, key))
